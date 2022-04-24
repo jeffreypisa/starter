882cc3d54212be923d7115d0e3947adb2fb21ba9
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2022-04-24 09:08:06 +0000

    More stuff

--- lib/acf.php
+++ lib/acf.php
@@ -13,38 +13,23 @@ function my_acf_json_save_point( $path ) {
     
 }
 
-function my_acf_flexible_content_layout_title( $title, $field, $layout, $i ) {
-	
-	
+add_filter('acf/fields/flexible_content/layout_title/name=stroken', 'my_acf_fields_flexible_content_layout_title', 10, 4);
+function my_acf_fields_flexible_content_layout_title( $title, $field, $layout, $i ) {
+
 	$oldtitle = $title;
 	
-	// remove layout title from text
-	$title = '';
 	
 	
 	// load text sub field
 	if( $text = get_sub_field('titel') ) {
-		
+		$title = '';
 		$title .= '<strong>' . $oldtitle . ' </strong> | ' . $text;
-	
-	} elseif( $text = get_sub_field('k1_titel') ) {
-		
-		$title .= '<strong>' . $oldtitle . ' </strong> | ' . $text;
-	
-	} elseif( $text = get_sub_field('k2_titel') ) {
-		
-		$title .= '<strong>' . $oldtitle . ' </strong> | ' . $text;
-	
+	} elseif( $text = get_sub_field('lead') ) {
+		$title = '';
+		$title .= '<strong>' . $oldtitle . ' </strong> | ' . $text;	
+	} else {
+		$title = '';
+		$title .= '<strong>' . $oldtitle . ' </strong>';	
 	}
-	else {
-		$title = '<strong>' . $oldtitle . ' </strong>';
-	}
-	
-	
-	// return
 	return $title;
-	
 }
-
-// name
-add_filter('acf/fields/flexible_content/layout_title/name=blokken', 'my_acf_flexible_content_layout_title', 10, 4);
